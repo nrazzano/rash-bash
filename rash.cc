da7274e4d675535b79e1819028f732dfa9255925
Nick J Razzano <nrazzano@euclid.nmu.edu>
2018-11-27 13:52:27 -0500

    11 points. set env. cd. ps1. run exe. search path (using execvp()). readline stuff. history log.

--- rash.cc
+++ rash.cc
@@ -1,25 +1,33 @@
 #include <iostream>
 #include <stdio.h>
+#include <stdlib.h>
 #include <readline/readline.h>
 #include <readline/history.h>
-#include <string.h>
+#include <string>
 #include <fstream> // write to file, using for time/date in history log
 #include <chrono>
 #include <ctime>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <vector>
+#include <queue>
+#include <algorithm> // for remove_if
+
 
 #define clear() printf("\033[H\033[J")
 
 using namespace std;
 
 void initRash(); // just for fun; got from 
-void cdCheck(const char* cmd, char* dir);
+bool cdCheck(const char* cmd, const char* dir);
 bool killRash(char* cmd);
 
+// in progress
+void splitString(string s, vector<string> v);
+
 // need to implement
-void splitString(string* s);
+void doFork();
 
 
 int main(int argc, const char * argv[]) {
@@ -33,23 +41,24 @@ int main(int argc, const char * argv[]) {
     char* buf;
     const char* command;
     char* args[10];
+    vector <string> strVec;
+    queue< vector <string> > commandQ;
+
 
 
     // for(int i = 0; i < 10; ++i) {
     //     args[i] = new char[255];
     // }
-
-    for (auto index : args) {
-        index = new char[255];
-    }
+    // for (auto index : args) {
+    //     index = new char[255];
+    // }
 
     using_history();
     read_history("history.txt");
+    string thing = "$ ";
 
-    while ((buf = readline("$ ")) != nullptr) {
+    while ((buf = readline(thing.c_str())) != nullptr) {
 
-        string str(buf);
-        // if (str.length() < 1) continue; 
 
         if (strlen(buf) > 0) {
             add_history(buf); // for arrow history
@@ -59,16 +68,49 @@ int main(int argc, const char * argv[]) {
 
         if (killRash(buf)) return 0;
 
-
         int num = sscanf(buf, "%ms %ms %ms", &args[0], &args[1], &args[2]);
         command = args[0];
 
 
-        cdCheck(command, args[1]);
+        string bufstr(buf);
+        splitString(bufstr, strVec);
+        // cout << bufstr << endl;
+        // vector <const char*> charVec(strVec.size(), nullptr);
 
+        // populate args from strVec
+        // for (int i = 0; i < strVec.size(); ++i) {
+        //     args[i] = strVec[i].c_str();
+        // }
+
+        if (cdCheck(command, args[1])) continue;
+
+
+
+
+
+        if (bufstr.find("=") != string::npos) { // means we should be setting an env var
+            int equalPos = bufstr.find("=");
+            string envVar = bufstr.substr(0, equalPos);
+            string envSetTo = bufstr.substr(equalPos + 1, bufstr.length());
+            if (envVar == "PS1") {
+                thing = envSetTo.append(thing);
+                continue;
+            }
+            else setenv(envVar.c_str(), envSetTo.c_str(), 1);
+        }
+        if (bufstr.find("$") != string::npos) {
+            int dollarPos = bufstr.find("$");
+            continue;
+        }
+
+
+
+
+
+
+        // TODO: Methodize fork stuff.
         int status = 0;
         pid_t pid = fork();
-
         if (pid < 0) { // fail
             cout << "fork pooped\n";
             return 0;
@@ -95,15 +137,15 @@ void initRash() {
     // sleep(1);
     // clear();
 }
-
-void cdCheck(const char* cmd, char* dir) {
+bool cdCheck(const char* cmd, const char* dir) {
     if (strcmp(cmd, "cd") == 0) {
         int ret = chdir(dir);
         if (ret == -1) cout << "poopy\n";
         else           cout << "changed to: " << dir << "\n";
+        return 1;
     }      
+    return 0;
 }
-
 bool killRash(char* cmd) {
     if (strcmp(cmd, "exit") == 0) {
         cout << "goodbye...\n";
@@ -112,6 +154,27 @@ bool killRash(char* cmd) {
     }
     return 0;
 }
+
+void splitString(string s, vector<string> v) {
+    size_t pos = 0;
+    string token;
+    
+    while ( ((pos = s.find(";")) != string::npos) || ((pos = s.find("&&")) != string::npos) ) {
+        token = s.substr(0, pos);
+        token.erase(remove_if(token.begin(), token.end(), ::isspace), token.end());
+        v.push_back(token);
+        // cout << token << endl;
+        // s.erase(0, s.find_first_not_of(' '));
+        s.erase(0, pos + 2);
+        // cout << s.find_first_not_of(' ') << endl;
+        // cout << s.at(s.find_first_not_of(' ')) << endl;
+
+    }
+    s.erase(remove_if(token.begin(), token.end(), ::isspace), token.end());
+    // cout << s << endl;
+    v.push_back(s);
+}
+
 
 // If I want to add the time to the history file?
 //  auto time = chrono::system_clock::now();
